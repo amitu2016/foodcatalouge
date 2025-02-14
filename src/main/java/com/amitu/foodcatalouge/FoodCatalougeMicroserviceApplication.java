1f7bea7aeecba063b7c66c7d453486f5a522d9f6
Amit Upadhyay <amitu@cdac.in>
2025-02-14 17:52:24 +0530

    commit

---
+++ src/main/java/com/amitu/foodcatalouge/FoodCatalougeMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.amitu.foodcatalouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCatalougeMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodCatalougeMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amitu/foodcatalouge/FoodCatalougeMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amitu.foodcatalouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCatalougeMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
